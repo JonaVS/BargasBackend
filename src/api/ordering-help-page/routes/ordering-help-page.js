2baa638b91120143d80c39e777ecd677a8421a27
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-05-20 15:22:48 -0600

    Feat: add single type for Help page via content builder

---
+++ src/api/ordering-help-page/routes/ordering-help-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ordering-help-page router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ordering-help-page.ordering-help-page');
---
+++ src/api/ordering-help-page/services/ordering-help-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ordering-help-page service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::ordering-help-page.ordering-help-page');
